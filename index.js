8aa78860b8fb1e899807c30159d9669a03fad2ab
USBrido <obrido@gmail.com>
2020-01-04 07:15:57 +0000

    Linted ad eternum

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-const { fetchCoordsByIp } = require('./iss');
-const { fetchMyIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
 const { nextISSTimesForMyLocation } = require('./iss');
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('162.245.144.188', (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned Coords:' , coords);
-// }); 
-
-// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-
-// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned flyover times:' , passTimes);
-// });
 
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
